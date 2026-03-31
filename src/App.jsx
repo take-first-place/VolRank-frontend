102329041950c62693e7c2157fa9440fb292162c
kim <chaehyun02kim@gmail.com>
2026-03-31 17:47:12 +0900

    feat: 로그인 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
-import Signup from './pages/Signup'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Signup from "./pages/Signup";
+import UserLoginPage from "./pages/UserLoginPage";
 
 const App = () => {
-
   return (
-    <Signup />
-  )
-}
+    <>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<UserLoginPage />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
