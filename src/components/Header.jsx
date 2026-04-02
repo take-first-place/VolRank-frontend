2b0317a44b923cd7bbb72ef5740dc593168c000a
kim <chaehyun02kim@gmail.com>
2026-04-02 15:54:16 +0900

    feat: 헤더 및 레이아웃 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,36 @@
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <header>
-            <h1>Header</h1>
-        </header>
-    )
+    <header className="header">
+      <div className="header-inner">
+        <Link to="/" className="logo">
+          VolRank
+        </Link>
+
+        <nav className="nav">
+          <Link to="/" className="nav-link">
+            홈 
+          </Link>
+          <Link to="/volunteers" className="nav-link">
+            봉사활동 
+          </Link>
+          <Link to="/ranking" className="nav-link">
+            랭킹 
+          </Link>
+        </nav>
+
+        <div className="auth-menu">
+          <Link to="/login" className="nav-link">
+            로그인 
+          </Link>
+          <Link to="/signup" className="signup-btn">
+            회원가입 
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import "./assets/global.css";
+import "./assets/layout.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
