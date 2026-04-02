5dc4d585105631b90934cc7334f0f242f643f233
min9gu <rudalsrn000@naver.com>
2026-04-02 10:28:08 +0900

    fix: App 라우팅 병합 및 마이페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Header from "./components/Header";
 import Signup from "./pages/Signup";
 import UserLoginPage from "./pages/UserLoginPage";
 import MyPage from "./pages/MyPage";
-import Header from "./components/Header";
 
-function App() {
+const App = () => {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<UserLoginPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </>
   );
-}
+};
 
 export default App;
