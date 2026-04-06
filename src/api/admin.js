5ba4f8d803c99a1d1052cfa5971c5a2fc96811fa
Jeong-yunyoung <lemonaa6685@gmail.com>
2026-04-06 16:55:37 +0900

    feat: adminapi

--- src/api/admin.js
+++ src/api/admin.js
@@ -9,6 +9,10 @@ export const  rejectUser = (id) =>{
     return api.post(`/admin/reject/${id}`);
 };
 
+export const  pendingUser = (id) =>{
+    return api.post(`/admin/pending/${id}`);
+};
+
 export const getCertificates = (id) =>{
     return api.get(`/admin/Certificates/${id}`);
 };
