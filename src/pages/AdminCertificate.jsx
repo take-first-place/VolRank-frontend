183f1d3ba01b6ae77e796cd4bb23bec51097e9c2
Jeong-yunyoung <lemonaa6685@gmail.com>
2026-04-07 14:02:42 +0900

    feat: 코드 수정

--- src/pages/AdminCertificate.jsx
+++ src/pages/AdminCertificate.jsx
@@ -1,21 +1,20 @@
-import { useEffect, useState } from "react";
+import {  useEffect, useState } from "react";
 import {approveUser, rejectUser, getCertificates} from "../../admin"
 
 
 const AdminCertificate = () => {
     const [list, setlist] = useState([]);
 
+   const fetchadmin =async () => {
+        const res = await getCertificates();
+        setlist(res.data);   
+    };
 
-const fetchadmin = async () => {
-    const res = await getCertificates();
-    setlist(res.data);
+useEffect (() => {
+    fetchadmin();
+    },[]);
 
-};
-
-useEffect (() => 
-    setTimeout(()=>
-    fetchadmin()), []); 
-
+  
 const handleApprove = async(id) =>{
     await approveUser(id);
     fetchadmin();
@@ -26,17 +25,47 @@ const handleReject = async(id) => {
     fetchadmin();
 };
 
+
+ 
+
 return(
-    <div>
-        <h2>승인 관리</h2>
-        {list.map((item) => (
-            <div key = {item.id}>
-            <span> {item.name} </span>
-            <button onClick={() => handleApprove(item.id)}>승인</button>
-            <button onClick={() => handleReject(item.id)}>반려</button>
+    <div className="card">
+      <h2>승인관리</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>이름</th>
+            <th>주소</th>
+            <th>제출날짜</th>
+            <th>상태</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.length === 0 ? (
+            <tr>
+              <td colSpan="5">데이터가 없습니다.</td>
+            </tr>
+          ) : (
+            list.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.submitted_at}</td>
+                <td>{item.status}</td>
+                <td>
+                  <div className="row">
+                   <button onClick={() => handleApprove(item.id)}>승인</button>
+                    <button onClick={() => handleReject(item.id)}>거부</button>
+                  </div>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
     </div>
-        ))}
-    </div>
+         
     );
 };
 export default AdminCertificate;
