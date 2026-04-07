1ce96f25eee26449761fd166dd33c46bdafffb02
Jeong-yunyoung <lemonaa6685@gmail.com>
2026-04-07 09:20:52 +0900

    관리지 api

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,18 +1,18 @@
 import api from "./axios";
 
 
-export const approveUser = (id) => {
-    return api.post(`/admin/approve/${id}`);
+export const approveUser = () => {
+    return api.post("/admin/approve/${id}");
 };
 
-export const  rejectUser = (id) =>{
-    return api.post(`/admin/reject/${id}`);
+export const  rejectUser = () =>{
+    return api.post("/admin/reject/${id}");
 };
 
-export const  pendingUser = (id) =>{
-    return api.post(`/admin/pending/${id}`);
+export const  pendingUser = () =>{
+    return api.post("/admin/pending/${id}");
 };
 
-export const getCertificates = (id) =>{
-    return api.get(`/admin/Certificates/${id}`);
+export const getCertificates = () =>{
+    return api.get("/admin/Certificates/${id}");
 };
